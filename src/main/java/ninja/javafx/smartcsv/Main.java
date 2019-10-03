3ce9ae8edd6bb8a2bedd101bc1378d177773b191
Andreas Billmann <andreas@billmann.de>
2019-10-03 21:08:29 +0200

    fixed the main javafx application problem

---
+++ src/main/java/ninja/javafx/smartcsv/Main.java
@@ -0,0 +1,10 @@
+package ninja.javafx.smartcsv;
+
+import ninja.javafx.smartcsv.fx.SmartCSV;
+
+public class Main {
+    public static void main(String[] args) {
+        // workaround for module problem
+        SmartCSV.main(args);
+    }
+}
